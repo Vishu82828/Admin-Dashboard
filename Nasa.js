46208eac5e6b7bf384f59344a4bc7bee0115cf04
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2024-10-24 14:13:43 +0530

    nasa api fetch

---
+++ Nasa.js
@@ -0,0 +1,21 @@
+let apiKey = "KMt9xHHhX09ZUTXBj35m7Lp3nmBRK5Q2Qo4NpIvF"
+let apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+let place = document.getElementById("container")
+fetch(apiUrl)
+    .then(data => data.json())
+    .then(item => {
+        console.log(item)
+        let media = `<img src="${item.url}" alt="${item.title}" class="apod-img" style="width:100%; height:auto;"></img>`
+        place.innerHTML = `
+            <div class="apod">
+                <h2>${item.title}</h2>
+                <p>${item.date}</p>
+                ${media}
+                <p>${item.explanation}</p>
+            </div>
+    `
+    })
+    .catch(error => {
+        console.log("Api not loading bro !!!!")
+        place.textContent = "NASA API is not Loading !!!!"
+    })
